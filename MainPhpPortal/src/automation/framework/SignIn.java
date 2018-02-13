462f6aa208cfa9b18ceecd872e58e46455ffd7cd
Ibe <ibedike@yahoo.com>
2018-02-13 15:16:57 +0000

    remove classes in default package

--- MainPhpPortal/src/automation/framework/SignIn.java
+++ MainPhpPortal/src/automation/framework/SignIn.java
@@ -2,6 +2,7 @@ package automation.framework;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import automation.framework.LoginPage;
 
 public class SignIn {
 	
